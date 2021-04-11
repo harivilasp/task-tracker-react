eebb7d8fdd7d0591ad988e9c528a1270fedaf7ee
Hari Vilas Panjwani <harivilasp@gmail.com>
2021-04-11 14:19:32 +0530

    Initialize project using Create React App

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export const Header = (props) => {
+    return (
+        <header>
+            <h1> Task Tracker</h1>
+            <h1> {props.title} </h1>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title:'Task Tracker',
+}
+
